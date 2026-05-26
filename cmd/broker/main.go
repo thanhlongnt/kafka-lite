6a3909b8aab197f3c532a1356c636b2e4dcee8e8
Chaitya Jodhavat <chaityajodhavat@gmail.com>
2026-05-25 21:09:23 -0700

    cli phase 2 work in progress

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 
@@ -9,9 +10,17 @@ import (
 
 func main() {
 	addr := flag.String("addr", ":9092", "gRPC listen address")
+	coordAddr := flag.String("coordinator", "", "coordinator gRPC address (phase 2, optional)")
 	flag.Parse()
 
 	b := broker.New()
+	if *coordAddr != "" {
+		if err := b.ConnectCoordinator(context.Background(), *coordAddr); err != nil {
+			log.Fatalf("connect coordinator: %v", err)
+		}
+		coordConn, _ := grpc.NewClient(*coordAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+
 	log.Printf("kafka-lite broker listening on %s", *addr)
 	if err := b.Serve(*addr); err != nil {
 		log.Fatalf("broker: %v", err)
